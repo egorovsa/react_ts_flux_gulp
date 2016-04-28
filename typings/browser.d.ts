e9d3227497587281592f0a1864d0cceffea7482f
egorovsa <shammani@yandex.ru>
2016-04-28 18:20:27 +0300

    comit

---
+++ typings/browser.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="browser/ambient/flux/index.d.ts" />
+/// <reference path="browser/ambient/history/index.d.ts" />
+/// <reference path="browser/ambient/lodash/index.d.ts" />
+/// <reference path="browser/ambient/react-dom/index.d.ts" />
+/// <reference path="browser/ambient/react/index.d.ts" />
---
+++ typings/main.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="main/ambient/flux/index.d.ts" />
+/// <reference path="main/ambient/history/index.d.ts" />
+/// <reference path="main/ambient/lodash/index.d.ts" />
+/// <reference path="main/ambient/react-dom/index.d.ts" />
+/// <reference path="main/ambient/react/index.d.ts" />
